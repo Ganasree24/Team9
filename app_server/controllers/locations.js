784988ea942ab8ba64012cf8a2694879797e562d
Ganasree24 <kganasree@gmail.com>
2024-10-14 20:11:27 +0530

    my first commit

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,36 +1,46 @@
 const homelist = (req, res) => {
-    res.render('locations-list', {
-      title:'Whole Sale Shops',
-      pageHeader: {
-        title: 'Whole Sale Shops',
-        strapline: 'sell goods in large quantities at lower prices'
+  res.render('locations-list', {
+    title: 'Wholesale Shops',
+    pageHeader: {
+      title: 'Wholesale Shops',
+      strapline: 'Your Trusted Source for Wholesale Recommendations',
+    },
+    sidebar: 'Discover the ultimate destination for wholesale shopping! Browse through our curated list of top-notch shops offering everything from groceries to garments. Take advantage of great deals, customer reviews, and essential facilities that make bulk buying a breeze. Start your journey to unbeatable prices and quality products today!',
+    locations: [
+      {
+        name: 'Charminar Textile Market',
+        address: 'Charminar, Hyderabad',
+        rating: 3,
+        facilities: ['Custom Stitching','Quality Assurance'],
+        link:'/location'
       },
-      sidebar: 'Wholesale shops, often referred to in the context of wholesale trade, are businesses that sell goods in large quantities at lower prices, primarily to retailers or other businesses rather than directly to consumers.' ,
-         locations:[{
-           name: 'Sanskriti whole sale',
-           address: 'General bazar,kalasiguda',
-           rating: 4,
-           facilities: ['kurthis','palazo'],
-           distance: '100m'
-         },{
-           name: 'SNS sarees and Dresses whole sale',
-           address: 'Tobacco bazar,secunderabad ',
-           rating: 3,
-           facilities: ['cotton', 'Designer', 'Tops skirts'],
-           distance: '200m'
-         },{
-           name: 'SLS mart whole sale',
-           address: 'Bodduppal,near Global champs school',
-           rating: 3,
-           facilities: [ 'All household goods and appliances'],
-           distance: '250m'
-         }]
-       });
-      };
+      {
+        name: 'FeelKnit Garments Wholesale',
+        address: 'Koti, Hyderabad',
+        rating: 3,
+        facilities: ['Custom garment labeling'],
+        link:'/shop1'
+      },
+      {
+        name: 'Begum Bazaar Grocery Wholesalers',
+        address: 'Begum Bazaar, Hyderabad',
+        rating: 3,
+        facilities: ['Bulk Packaging', 'Organic Options'],
+        link:'/shop2'
+      },
+    ],
+  });
+};
          /* GET 'Location info' page */
     const locationInfo = (req, res) => {
        res.render('location-info', { title: 'Location info' });
       };
+      const shop1 = (req, res) => {
+        res.render('shop1', { title: 'shop1' });
+       };
+       const shop2 = (req, res) => {
+        res.render('shop2', { title: 'shop2' });
+       };
       /* GET 'Add review' page */
       const addReview = (req, res) => {
        res.render('location-review-form', { title: 'Add review' });
@@ -38,6 +48,8 @@ const homelist = (req, res) => {
       module.exports = {
        homelist,
        locationInfo,
-       addReview
+       addReview,
+       shop1,
+       shop2
       };
  
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -6,6 +6,8 @@ const ctrlOthers = require('../controllers/others');
 /* Locations pages */
 router.get('/', ctrlLocations.homelist);
 router.get('/location', ctrlLocations.locationInfo);
+router.get('/shop1', ctrlLocations.shop1);
+router.get('/shop2', ctrlLocations.shop2);
 router.get('/location/review/new', ctrlLocations.addReview);
 /* Other pages */
 router.get('/about', ctrlOthers.about);
